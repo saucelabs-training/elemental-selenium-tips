122a003630a86e267b2e3c055dd279d05f8263ab
RomanIsko <iskoroman@gmail.com>
2015-03-20 14:19:16 +0200

    added Java work with basic authentication example #2

---
+++ 13-work-with-basic-auth/java/WorkWithBasicAuth2.java
@@ -0,0 +1,31 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+
+public class WorkWithBasicAuth2 {
+    WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        driver.get("http://admin:admin@the-internet.herokuapp.com/basic_auth");
+    }
+
+    @Test
+    public void workWithBasicAuthTest2() {
+        driver.get("http://the-internet.herokuapp.com/basic_auth");
+        String pageMessage = driver.findElement(By.cssSelector("p")).getText();
+        assertThat(pageMessage, containsString("Congratulations!"));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+}
