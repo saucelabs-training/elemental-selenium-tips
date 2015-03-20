bd3d9fc0b1e0641d180263aad65bf4c2e3a11347
RomanIsko <iskoroman@gmail.com>
2015-03-20 14:05:46 +0200

    add download a file revisited Java example

---
+++ 08-download-a-file-revisited/java/DownloadFileRevisited.java
@@ -0,0 +1,45 @@
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+
+public class DownloadFileRevisited {
+    WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+    }
+
+    @Test
+    public void dropdownTest() throws IOException {
+        driver.get("http://the-internet.herokuapp.com/download");
+        String link = driver.findElement(By.cssSelector(".example a:nth-of-type(2)")).getAttribute("href");
+
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpGet request = new HttpGet(link);
+        HttpResponse response = httpClient.execute(request);
+        String contentType = response.getFirstHeader("Content-Type").getValue();
+        int contentLength = Integer.parseInt(response.getFirstHeader("Content-Length").getValue());
+
+        assertThat(contentType, is("application/pdf"));
+        assertThat(contentLength, greaterThan(0));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+}
