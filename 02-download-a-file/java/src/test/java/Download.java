92cac836ec198e26dae92ff14025020961f1f93c
Dave Haeffner <dhaeffner@gmail.com>
2015-10-23 17:46:38 -0400

    Added a pom.xml and initial Maven directory structure. Added file/directory deletion in the test teardown. Switched the Matchers to use Hamcrest's CoreMatchers (that ship with JUnit). Added inline comments to illustrate the advantages over AssertTrue and linked to documentation in the JUnit GitHub Wiki (which will be useful when writing the tip). Also commented out import statements that are used only with ChromeDriver.

--- 02-download-a-file/java/src/test/java/Download.java
+++ 02-download-a-file/java/src/test/java/Download.java
@@ -1,21 +1,23 @@
+import org.hamcrest.core.IsNot;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import static org.hamcrest.CoreMatchers.*;
+// Chrome import statements
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeOptions;
+//import java.util.HashMap;
+//import java.util.Map;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
-import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class Download {
     WebDriver driver;
@@ -25,11 +27,13 @@ public class Download {
     public void setUp() throws Exception {
         folder = new File(UUID.randomUUID().toString());
         folder.mkdir();
+
 //        Firefox
         FirefoxProfile profile = new FirefoxProfile();
         profile.setPreference("browser.download.dir", folder.getAbsolutePath());
         profile.setPreference("browser.download.folderList", 2);
-        profile.setPreference("browser.helperApps.neverAsk.saveToDisk", "image/jpeg, application/pdf");
+        profile.setPreference("browser.helperApps.neverAsk.saveToDisk",
+                "image/jpeg, application/pdf, application/octet-stream");
         profile.setPreference("pdfjs.disabled", true);
         driver = new FirefoxDriver(profile);
 
@@ -49,17 +53,29 @@ public class Download {
     public void download() throws Exception {
         driver.get("http://the-internet.herokuapp.com/download");
         driver.findElement(By.cssSelector(".example a")).click();
-//        wait 2 seconds to download
+        // Wait 2 seconds to download file
         Thread.sleep(2000);
         File[] listOfFiles = folder.listFiles();
-        assertThat(listOfFiles.length, greaterThan(0));
+        assertTrue(listOfFiles.length > 0);
         for (File file : listOfFiles) {
-            assertThat(file.length(), greaterThan((long) 0));
+            //assertTrue(file.length() > 0);
+            // assertTrue does not offer much to go on when there's a failure
+            // Thankfully JUnit ships with Hamcrest CoreMatchers
+            // https://github.com/junit-team/junit/wiki/Matchers-and-assertthat
+            assertThat(file.length(), not((long) 0));
+            // outputs:
+            // java.lang.AssertionError:
+            // Expected: not <0L>
+            //      but: was <0L>
         }
     }
 
     @After
     public void tearDown() throws Exception {
         driver.quit();
+        for (File file: folder.listFiles()) {
+            file.delete();
+        }
+        folder.delete();
     }
 }
