2cdce0b86036839a2c43b795664725fb0f46b069
Dave Haeffner <dhaeffner@gmail.com>
2015-10-27 19:03:14 -0400

    Added Maven structure and swapped out Hamcrest matchers

--- 13-work-with-basic-auth/java/src/test/java/WorkWithBasicAuth.java
+++ 13-work-with-basic-auth/java/src/test/java/WorkWithBasicAuth.java
@@ -4,22 +4,20 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
 
-public class WorkWithBasicAuth2 {
+public class WorkWithBasicAuth {
     WebDriver driver;
 
     @Before
     public void setUp() throws Exception {
         driver = new FirefoxDriver();
-        driver.get("http://admin:admin@the-internet.herokuapp.com/basic_auth");
     }
 
     @Test
-    public void workWithBasicAuthTest2() {
-        driver.get("http://the-internet.herokuapp.com/basic_auth");
+    public void workWithBasicAuthTest() {
+        driver.get("http://admin:admin@the-internet.herokuapp.com/basic_auth");
         String pageMessage = driver.findElement(By.cssSelector("p")).getText();
         assertThat(pageMessage, containsString("Congratulations!"));
     }
--- 13-work-with-basic-auth/java/src/test/java/WorkWithBasicAuth2.java
+++ 13-work-with-basic-auth/java/src/test/java/WorkWithBasicAuth2.java
@@ -4,21 +4,21 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
 
-public class WorkWithBasicAuth {
+public class WorkWithBasicAuth2 {
     WebDriver driver;
 
     @Before
     public void setUp() throws Exception {
         driver = new FirefoxDriver();
+        driver.get("http://admin:admin@the-internet.herokuapp.com/basic_auth");
     }
 
     @Test
-    public void workWithBasicAuthTest() {
-        driver.get("http://admin:admin@the-internet.herokuapp.com/basic_auth");
+    public void workWithBasicAuthTest2() {
+        driver.get("http://the-internet.herokuapp.com/basic_auth");
         String pageMessage = driver.findElement(By.cssSelector("p")).getText();
         assertThat(pageMessage, containsString("Congratulations!"));
     }
