bbd925bcf457c3bffe7bab0893cfb4a61966358f
Dave Haeffner <dhaeffner@gmail.com>
2015-11-07 19:48:27 -0500

    Reordered import statement

--- 02-download-a-file/java/src/test/java/Download.java
+++ 02-download-a-file/java/src/test/java/Download.java
@@ -1,8 +1,8 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 // Chrome import statements
