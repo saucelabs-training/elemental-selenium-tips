953e49c416d8c77a1e852ffdb2f925ef69b94225
Dave Haeffner <dhaeffner@gmail.com>
2015-11-10 16:33:59 -0500

    Added an initial test to mimic a poor man's way to accomplish selecting from a dropdown (per the tip write-up). Also renamed the list variable in the 2nd test from `select` to `selectList` for readibility.

--- 05-select-from-a-dropdown/java/src/test/java/Dropdown.java
+++ 05-select-from-a-dropdown/java/src/test/java/Dropdown.java
@@ -3,8 +3,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -16,17 +21,37 @@ public class Dropdown {
         driver = new FirefoxDriver();
     }
 
-    @Test
-    public void dropdownTest() {
-        driver.get("http://the-internet.herokuapp.com/dropdown");
-        Select select = new Select(driver.findElement(By.id("dropdown")));
-        select.selectByVisibleText("Option 1");
-        // You could also use select.selectByValue("1");
-        assertThat(select.getFirstSelectedOption().getText(), is(equalTo("Option 1")));
-    }
-
     @After
     public void tearDown() throws Exception {
         driver.quit();
     }
+
+    @Test
+    public void dropdownTest() {
+        driver.get("http://the-internet.herokuapp.com/dropdown");
+        WebElement dropdownList = driver.findElement(By.id("dropdown"));
+        List<WebElement> options = dropdownList.findElements(By.tagName("option"));
+        for (int i = 0; i < options.size(); i++) {
+            if (options.get(i).getText().equals("Option 1")) {
+                options.get(i).click();
+            }
+        }
+        String selectedOption = "";
+        for (int i = 0; i < options.size(); i++) {
+            if (options.get(i).isSelected()) {
+                selectedOption = options.get(i).getText();
+            }
+        }
+        assertThat(selectedOption, is("Option 1"));
+    }
+
+    @Test
+    public void dropdownTestRedux() {
+        driver.get("http://the-internet.herokuapp.com/dropdown");
+        Select selectList = new Select(driver.findElement(By.id("dropdown")));
+        selectList.selectByVisibleText("Option 1");
+        // You could also use select.selectByValue("1");
+        assertThat(selectList.getFirstSelectedOption().getText(), is(equalTo("Option 1")));
+    }
+
 }
