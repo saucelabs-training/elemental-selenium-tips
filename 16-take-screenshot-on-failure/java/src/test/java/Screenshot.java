d5d86eb4829ae15d68128828ab8b63d179dc2e28
Dave Haeffner <dhaeffner@gmail.com>
2015-11-11 13:40:56 -0500

    Added simple Maven structure and pom.xml. Renamed test file/class to be more descriptive. Swapped out the try/catch approach for a JUnit Rule so it applies to all test failures. Removed timestamp filenaming in favor of a simpler naming scheme with the test class name and test method name. Moved the teardown into the Rule to get the timing right. Added matchers, updated/simplified the assertion, and made it so the assertion is forced to fail. Example screenshot included.

---
+++ 16-take-screenshot-on-failure/java/src/test/java/Screenshot.java
@@ -0,0 +1,53 @@
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class Screenshot {
+    WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+    }
+
+    @Rule
+    public TestRule watcher = new TestWatcher() {
+        @Override
+        protected void failed(Throwable throwable, Description description) {
+            File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            try {
+                FileUtils.copyFile(scrFile,
+                        new File("failshot_"
+                                + description.getClassName()
+                                + "_" + description.getMethodName()
+                                + ".png"));
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        }
+
+        @Override
+        protected void finished(Description description) {
+            driver.quit();
+        }
+    };
+
+    @Test
+    public void OnError() {
+        driver.get("http://the-internet.herokuapp.com");
+        assertThat(false, is(true));
+    }
+
+}
