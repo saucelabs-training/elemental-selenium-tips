6eb61e2f3628f7c0fe875c4fc2b7542da2479483
Dave Haeffner <dhaeffner@gmail.com>
2015-11-11 15:13:50 -0500

    Added initial Maven structure and renamed file to be more descriptive. Pulled in matchers for better readibility and stacktrace output. Fixed a whole host of issues with regards to missing Selenium commands, incorrect locators, and broken sort checking. Also added some comments to demistify some of what's going on in the sort check (specifically the lexographic check).

---
+++ 25-tables/java/src/test/java/Tables.java
@@ -0,0 +1,95 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import java.util.LinkedList;
+import java.util.List;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.number.OrderingComparison.lessThan;
+import static org.hamcrest.number.OrderingComparison.lessThanOrEqualTo;
+import static org.hamcrest.number.OrderingComparison.greaterThanOrEqualTo;
+
+public class Tables {
+    WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+
+    @Test
+    public void withoutHelpfulMarkup()
+    {
+        driver.get("http://the-internet.herokuapp.com/tables");
+
+        // sort dues column in ascending order
+        driver.findElement(By.cssSelector("#table1 thead tr th:nth-of-type(4)")).click();
+
+        // get values from dues column (w/o $)
+        List<WebElement> dues = driver.findElements(By.cssSelector("#table1 tbody tr td:nth-of-type(4)"));
+        List<Double> dueValues = new LinkedList<Double>();
+        for(WebElement element : dues){
+            dueValues.add(Double.parseDouble(element.getText().replace("$", "")));
+        }
+
+        // check that dues are in ascending order
+        for(int counter = 0; counter < dueValues.size() - 1; counter++){
+            assertThat(dueValues.get(counter), is(lessThanOrEqualTo(dueValues.get(counter + 1))));
+        }
+
+        // sort dues column in descending order
+        driver.findElement(By.cssSelector("#table1 thead tr th:nth-of-type(4)")).click();
+
+        // get values from dues column (w/o $) again
+        dues = driver.findElements(By.cssSelector("#table1 tbody tr td:nth-of-type(4)"));
+        dueValues = new LinkedList<Double>();
+        for(WebElement element : dues){
+            dueValues.add(Double.parseDouble(element.getText().replace("$", "")));
+        }
+
+        // assert dues are in descending order
+        for(int counter = 0; counter < dueValues.size() - 1; counter++){
+            assertThat(dueValues.get(counter), is(greaterThanOrEqualTo(dueValues.get(counter + 1))));
+        }
+
+        // sort email column in ascending order
+        driver.findElement(By.cssSelector("#table1 thead tr th:nth-of-type(3)")).click();
+
+        // get values from email column
+        List<WebElement> emails = driver.findElements(By.cssSelector("#table1 tbody tr td:nth-of-type(3)"));
+
+        // assert emails are in ascending order
+        for(int counter = 0; counter < emails.size() -1; counter++){
+            assertThat(
+                    emails.get(counter).getText().compareTo(emails.get(counter + 1).getText()),
+                    is(lessThan(0)));
+            // checking for a negative number for ascending order
+            // would check for a positive number if descending
+        }
+    }
+
+    @Test
+    public void withHelpfulMarkup()
+    {
+        driver.get("http://the-internet.herokuapp.com/tables");
+        driver.findElement(By.cssSelector("#table2 thead .dues")).click();
+        List<WebElement> dues = driver.findElements(By.cssSelector("#table2 tbody tr td:nth-of-type(4)"));
+        List<Double> dueValues = new LinkedList<Double>();
+        for(WebElement element : dues){
+            dueValues.add(Double.parseDouble(element.getText().replace("$", "")));
+        }
+        for(int counter = 0; counter < dueValues.size() - 1; counter++){
+            assertThat(dueValues.get(counter), is(lessThanOrEqualTo(dueValues.get(counter + 1))));
+        }
+    }
+}
