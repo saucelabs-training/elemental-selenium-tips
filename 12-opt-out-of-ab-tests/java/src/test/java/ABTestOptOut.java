9ff5411f2c3a4fe8483819b5d3a24e60178e4c99
Dave Haeffner <dhaeffner@gmail.com>
2015-11-11 11:43:04 -0500

    Added simple Maven structure with pom.xml for depdencies. Renamed the file to be descriptive. Added matchers, updated assertions, and added missing bits to align with the tip as written.

--- 12-opt-out-of-ab-tests/java/src/test/java/ABTestOptOut.java
+++ 12-opt-out-of-ab-tests/java/src/test/java/ABTestOptOut.java
@@ -1,17 +1,14 @@
-package num12;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
-/**
- * Created by andrew on 8/22/15.
- */
-public class example {
+public class ABTestOptOut {
 
     WebDriver driver;
 
@@ -26,28 +23,29 @@ public class example {
     }
 
     @Test
-    public void TestSwitchingCookie() {
+    public void WithCookieAfterVisitingPage() {
         driver.get("http://the-internet.herokuapp.com/abtest");
         String headingText = driver.findElement(By.tagName("h3")).getText();
-        assert (headingText.equals("A/B Test Variation 1") || headingText.equals("A/B Test Control"));
+        assertThat(headingText, startsWith("A/B Test"));
         driver.manage().addCookie(new Cookie("optimizelyOptOut", "true"));
         driver.navigate().refresh();
         headingText = driver.findElement(By.cssSelector("h3")).getText();
-        assert headingText.equals("No A/B Test");
+        assertThat(headingText, is("No A/B Test"));
     }
 
     @Test
-    public void TestCookie() {
+    public void WithCookieBeforeVisitingPage() {
+        driver.get("http://the-internet.herokuapp.com");
         driver.manage().addCookie(new Cookie("optimizelyOptOut", "true"));
         driver.get("http://the-internet.herokuapp.com/abtest");
-        assert driver.findElement(By.cssSelector("h3")).getText().equals("No A/B Test");
+        assertThat(driver.findElement(By.cssSelector("h3")).getText(), is("No A/B Test"));
     }
 
     @Test
-    public void TestOptOutViaUrl() {
+    public void WithOptOutUrl() {
         driver.get("http://the-internet.herokuapp.com/abtest?optimizely_opt_out=true");
-        assert driver.findElement(By.cssSelector("h3")).getText().equals("No A/B Test");
+        driver.switchTo().alert().dismiss();
+        assertThat(driver.findElement(By.cssSelector("h3")).getText(), is("No A/B Test"));
     }
-
 
 }
