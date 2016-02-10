bfd8eb6283fc3f8c994c7d7b9dbd2c5c5717515e
Mike Millgate <mikemillgate@gmail.com>
2016-02-10 05:56:30 -0700

    updated based on @Dude-X and @bittner feedback
    thanks for the tips

--- 05-select-from-a-dropdown/python/es5_dropdown.py
+++ 05-select-from-a-dropdown/python/es5_dropdown.py
@@ -1,16 +1,22 @@
 # http://elementalselenium.com/tips/5-select-from-a-dropdown
 import unittest
 from selenium import webdriver
-from selenium.webdriver.support.select import Select as WebDriverSelect # for test_example_2
+# for test_example_2
+from selenium.webdriver.support.select import Select as WebDriverSelect
+
 
 class ES5_DropDown(unittest.TestCase):
-    
+
     def setUp(self):
         self.driver = webdriver.Firefox()
 
+    def tearDown(self):
+        self.driver.quit()
+
     def test_example_1(self):
-        self.driver.get('http://the-internet.herokuapp.com/dropdown')
-        dropdown_list = self.driver.find_element_by_id('dropdown')
+        driver = self.driver
+        driver.get('http://the-internet.herokuapp.com/dropdown')
+        dropdown_list = driver.find_element_by_id('dropdown')
         options = dropdown_list.find_elements_by_tag_name('option')
         for opt in options:
             if opt.text == 'Option 1':
@@ -18,15 +24,16 @@ class ES5_DropDown(unittest.TestCase):
         for opt in options:
             if opt.is_selected():
                 selected_option = opt.text
-        assert(selected_option == 'Option 1')
+        assert selected_option == 'Option 1'
 
     def test_example_2(self):
-        self.driver.get('http://the-internet.herokuapp.com/dropdown')
-        dropdown = self.driver.find_element_by_id('dropdown')
+        driver = self.driver
+        driver.get('http://the-internet.herokuapp.com/dropdown')
+        dropdown = driver.find_element_by_id('dropdown')
         select_list = WebDriverSelect(dropdown)
         select_list.select_by_visible_text('Option 1')
         selected_option = select_list.first_selected_option.text
-        assert(selected_option == 'Option 1')
+        assert selected_option == 'Option 1'
 
-    def tearDown(self):
-        self.driver.quit()
+if __name__ == "__main__":
+    unittest.main()
