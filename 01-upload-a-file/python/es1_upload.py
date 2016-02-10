5736ccf440a5263e2b1641ad4a9a19180ee5a6ac
Mike Millgate <mikemillgate@gmail.com>
2016-02-10 05:53:16 -0700

    updated based on @Dude-X and @bittner feedback
    thanks for the tips

--- 01-upload-a-file/python/es1_upload.py
+++ 01-upload-a-file/python/es1_upload.py
@@ -2,22 +2,26 @@
 import os
 import unittest
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys # for send_keys
+
 
 class ES1_Upload(unittest.TestCase):
-    
+
     def setUp(self):
         self.driver = webdriver.Firefox()
 
-    def test_example_1(self):
-        filename = 'some-file.txt'
-        file = os.path.join(os.getcwd(), filename)
-        self.driver.get('http://the-internet.herokuapp.com/upload')
-        self.driver.find_element_by_id('file-upload').send_keys(file)
-        self.driver.find_element_by_id('file-submit').click()
-        
-        uploaded_file = self.driver.find_element_by_id('uploaded-files').text
-        assert(uploaded_file == filename)
-
     def tearDown(self):
         self.driver.quit()
+
+    def test_example_1(self):
+        driver = self.driver
+        filename = 'some-file.txt'
+        file = os.path.join(os.getcwd(), filename)
+        driver.get('http://the-internet.herokuapp.com/upload')
+        driver.find_element_by_id('file-upload').send_keys(file)
+        driver.find_element_by_id('file-submit').click()
+
+        uploaded_file = driver.find_element_by_id('uploaded-files').text
+        assert uploaded_file == filename
+
+if __name__ == "__main__":
+    unittest.main()
