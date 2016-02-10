c298c8bc779e9a5858abf262b0f211cad441e994
Mike Millgate <mikemillgate@gmail.com>
2016-02-10 05:55:41 -0700

    updated based on @Dude-X and @bittner feedback
    thanks for the tips

--- 04-work-with-multiple-windows/python/es4_windows.py
+++ 04-work-with-multiple-windows/python/es4_windows.py
@@ -1,35 +1,39 @@
 # http://elementalselenium.com/tips/4-work-with-multiple-windows
-import os
 import unittest
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys # for send_keys
+
 
 class ES4_Windows(unittest.TestCase):
-    
+
     def setUp(self):
         self.driver = webdriver.Firefox()
 
+    def tearDown(self):
+        self.driver.quit()
+
     def test_example_1(self):
-        self.driver.get('http://the-internet.herokuapp.com/windows')
-        self.driver.find_element_by_css_selector('.example a').click()
-        self.driver.switch_to_window(self.driver.window_handles[0])
-        assert(self.driver.title != "New Window")
-        self.driver.switch_to_window(self.driver.window_handles[-1])
-        assert(self.driver.title == "New Window")
-        
+        driver = self.driver
+        driver.get('http://the-internet.herokuapp.com/windows')
+        driver.find_element_by_css_selector('.example a').click()
+        driver.switch_to_window(driver.window_handles[0])
+        assert driver.title != "New Window"
+        driver.switch_to_window(driver.window_handles[-1])
+        assert driver.title == "New Window"
+
     def test_example_2(self):
-        self.driver.get('http://the-internet.herokuapp.com/windows')
-       
-        first_window = self.driver.window_handles[0]
-        self.driver.find_element_by_css_selector('.example a').click()
-        all_windows = self.driver.window_handles
+        driver = self.driver
+        driver.get('http://the-internet.herokuapp.com/windows')
+
+        first_window = driver.window_handles[0]
+        driver.find_element_by_css_selector('.example a').click()
+        all_windows = driver.window_handles
         for window in all_windows:
             if window != first_window:
                 new_window = window
-        self.driver.switch_to_window(first_window)
-        assert(self.driver.title != "New Window")
-        self.driver.switch_to_window(new_window)
-        assert(self.driver.title == "New Window")
-        
-    def tearDown(self):
-        self.driver.quit()
+        driver.switch_to_window(first_window)
+        assert driver.title != "New Window"
+        driver.switch_to_window(new_window)
+        assert driver.title == "New Window"
+
+if __name__ == "__main__":
+    unittest.main()
