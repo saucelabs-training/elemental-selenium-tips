d07680f8cff6413890821af0fd28520a95f4d2b7
Dave Haeffner <dhaeffner@gmail.com>
2016-06-08 16:35:20 -0400

    Added C# example for downloading a file

---
+++ 02-download-a-file/csharp/FileDownload.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System.IO;
+using System.Threading;
+
+namespace FileDownload
+{
+    public class FileDownload
+    {
+        IWebDriver Driver;
+        string FolderPath;
+
+        [SetUp]
+        public void SetUp()
+        {
+            FolderPath = @"C:\Temp\" + System.Guid.NewGuid().ToString();
+            Directory.CreateDirectory(FolderPath);
+
+            FirefoxProfile profile = new FirefoxProfile();
+            profile.SetPreference("browser.download.dir", FolderPath);
+            profile.SetPreference("browser.download.folderList", 2);
+            profile.SetPreference("browser.helperApps.neverAsk.saveToDisk",
+                                  "image/jpeg, application/pdf, application/octet-stream");
+            profile.SetPreference("pdfjs.disabled", true);
+            Driver = new FirefoxDriver(profile);
+        }
+
+        [Test]
+        public void DownloadFileToDisk()
+        {
+            Driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/download");
+            Driver.FindElement(By.CssSelector(".example a")).Click();
+            Thread.Sleep(2000);
+
+            DirectoryInfo DownloadFolder = new DirectoryInfo(FolderPath);
+            Assert.IsTrue(DownloadFolder.GetFiles().Length > 0, "File not downloaded");
+            foreach(FileInfo file in DownloadFolder.GetFiles())
+            {
+                Assert.IsTrue(file.Length > 0, "File empty");
+            }
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.Quit();
+            Directory.Delete(FolderPath, true);
+        }
+
+
+    }
+}
