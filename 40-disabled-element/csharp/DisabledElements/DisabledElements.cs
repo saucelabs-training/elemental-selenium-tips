466b6ba0c77468838bbd2c24784d0755d24117c0
Dave Haeffner <dhaeffner@gmail.com>
2016-06-09 15:51:33 -0400

    Added tip 40 for C#

---
+++ 40-disabled-element/csharp/DisabledElements/DisabledElements.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace DisabledElements
+{
+    public class DisabledElements
+    {
+        IWebDriver Driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Driver = new FirefoxDriver();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.Quit();
+        }
+
+        [Test]
+        public void ElementIsDisabled()
+        {
+            Driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/dropdown");
+            var Dropdown = new SelectElement(Driver.FindElement(By.Id("dropdown")));
+            // The SelectedOption we want is selected by default on page load
+            Assert.False(Dropdown.SelectedOption.Enabled);
+        }
+    }
+}
