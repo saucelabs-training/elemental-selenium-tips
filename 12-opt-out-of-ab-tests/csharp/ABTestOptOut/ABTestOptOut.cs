8279ce7a47f4755233d44a5f768e71fec5bc18ca
Dave Haeffner <dhaeffner@gmail.com>
2016-06-09 14:04:12 -0400

    Added tip 12 for C#

---
+++ 12-opt-out-of-ab-tests/csharp/ABTestOptOut/ABTestOptOut.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+
+namespace ABTestOptOut
+{
+    public class ABTestOptOut
+    {
+        IWebDriver Driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Driver = new FirefoxDriver();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.Quit();
+        }
+
+        [Test]
+        public void OptOutWithCookieAfterVisitingPage()
+        {
+            Driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/abtest");
+            string HeadingText = Driver.FindElement(By.TagName("h3")).Text;
+            Assert.That(HeadingText.StartsWith("A/B Test"));
+            Driver.Manage().Cookies.AddCookie(new Cookie("optimizelyOptOut", "true"));
+            Driver.Navigate().Refresh();
+            HeadingText = Driver.FindElement(By.TagName("h3")).Text;
+            Assert.That(HeadingText.StartsWith("No A/B Test"));
+        }
+
+        [Test]
+        public void OptOutWithCookieBeforeVisitingPage()
+        {
+            Driver.Navigate().GoToUrl("http://the-internet.herokuapp.com");
+            Driver.Manage().Cookies.AddCookie(new Cookie("optimizelyOptOut", "true"));
+            Driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/abtest");
+            string HeadingText = Driver.FindElement(By.TagName("h3")).Text;
+            Assert.That(HeadingText.StartsWith("No A/B Test"));
+        }
+
+        [Test]
+        public void OptOutWithURL()
+        {
+            Driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/abtest?optimizely_opt_out=true");
+            Driver.SwitchTo().Alert().Dismiss();
+            string HeadingText = Driver.FindElement(By.TagName("h3")).Text;
+            Assert.That(HeadingText.StartsWith("No A/B Test"));
+        }
+    }
+}
