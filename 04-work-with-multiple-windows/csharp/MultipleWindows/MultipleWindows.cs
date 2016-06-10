53220fdaa23752bc915190b2065da84c86c7af39
Dave Haeffner <dhaeffner@gmail.com>
2016-06-10 15:02:03 -0400

    Added C# code for tip 4

---
+++ 04-work-with-multiple-windows/csharp/MultipleWindows/MultipleWindows.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System.Collections.Generic;
+
+public class MultipleWindows
+{
+    IWebDriver Driver;
+
+    [SetUp]
+    public void SetUp()
+    {
+        Driver = new FirefoxDriver();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Driver.Quit();
+    }
+
+    [Test]
+    public void MultipleWindowsExample1()
+    {
+        Driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/windows");
+        Driver.FindElement(By.CssSelector(".example a")).Click();
+        var Windows = Driver.WindowHandles;
+
+        Driver.SwitchTo().Window(Windows[0]);
+        Assert.That(Driver.Title != "New Window");
+
+        Driver.SwitchTo().Window(Windows[1]);
+        Assert.That(Driver.Title.Equals("New Window"));
+    }
+
+    [Test]
+    public void MultipleWindowsExample2()
+    {
+        Driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/windows");
+        string FirstWindow = Driver.CurrentWindowHandle;
+        string SecondWindow = "";
+
+        Driver.FindElement(By.CssSelector(".example a")).Click();
+
+        var Windows = Driver.WindowHandles;
+        foreach(var Window in Windows)
+        {
+            if (Window != FirstWindow)
+                SecondWindow = Window;
+        }
+
+        Driver.SwitchTo().Window(FirstWindow);
+        Assert.That(Driver.Title != "New Window");
+
+        Driver.SwitchTo().Window(SecondWindow);
+        Assert.That(Driver.Title.Equals("New Window"));
+    }
+}
