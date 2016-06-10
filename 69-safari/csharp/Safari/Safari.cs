7d5615cb72ae8914b2f2302fbbba9208c280bfc7
Dave Haeffner <dhaeffner@gmail.com>
2016-06-10 15:03:46 -0400

    Added C# code for tip 69

---
+++ 69-safari/csharp/Safari/Safari.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Safari;
+
+public class Safari
+{
+    IWebDriver Driver;
+
+    [SetUp]
+    public void SetUp()
+    {
+        Driver = new SafariDriver();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Driver.Quit();
+    }
+
+    [Test]
+    public void SimpleTest()
+    {
+        Driver.Navigate().GoToUrl("http://the-internet.herokuapp.com");
+        Assert.That(Driver.Title.Equals("The Internet"));
+    }
+}
