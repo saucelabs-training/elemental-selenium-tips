146abf6873c4f781536ba844707bd652f7cdfd54
Dave Haeffner <dhaeffner@gmail.com>
2016-06-13 11:12:04 -0400

    Removed unnecessary explicit wait from tip 50 C# example

--- 50-hovers/csharp/Hovers.cs
+++ 50-hovers/csharp/Hovers.cs
@@ -2,8 +2,6 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Interactions;
-using OpenQA.Selenium.Support.UI;
-using System;
 
 public class Hovers
 {
@@ -26,16 +24,11 @@ public class Hovers
     {
         Driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/hovers");
 
-        // Hover over desired element
         IWebElement Avatar = Driver.FindElement(By.ClassName("figure"));
         Actions Builder = new Actions(Driver);
         Builder.MoveToElement(Avatar).Build().Perform();
 
-        // Wait until the hover appears
         By Hover = By.ClassName("figcaption");
-        WebDriverWait Wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(5));
-        Wait.Until(ExpectedConditions.ElementIsVisible(Hover));
-
         Assert.That(Driver.FindElement(Hover).Displayed);
     }
 }
