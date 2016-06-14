bb789b6d0e78aa6696e5441f8c6007a50264eded
Dave Haeffner <dhaeffner@gmail.com>
2016-06-13 22:41:37 -0400

    Added C# code for tip 29

---
+++ 29-chrome-driver/csharp/Chrome/Chrome.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+
+public class Chrome
+{
+    IWebDriver Driver;
+    string VendorDirectory = Directory.GetParent(
+        Path.GetDirectoryName(typeof(Chrome).Assembly.Location)).
+            Parent.FullName + @"\Vendor";
+
+    [SetUp]
+    public void SetUp()
+    {
+        Driver = new ChromeDriver();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Driver.Quit();
+    }
+
+    [Test]
+    public void PageLoads()
+    {
+        Driver.Navigate().GoToUrl("http://the-internet.herokuapp.com");
+        Assert.That(Driver.Title.Equals("The Internet"));
+    }
+}
