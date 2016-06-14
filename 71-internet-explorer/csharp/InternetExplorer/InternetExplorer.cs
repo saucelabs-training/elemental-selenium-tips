728db9eb7aec857b57b3b51a206a4ca6a10f6ab4
Dave Haeffner <dhaeffner@gmail.com>
2016-06-13 20:42:38 -0400

    C# code for tip 71

---
+++ 71-internet-explorer/csharp/InternetExplorer/InternetExplorer.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using System.IO;
+
+namespace InternetExplorer
+{
+    public class InternetExplorer
+    {
+        IWebDriver Driver;
+        string VendorDirectory = Directory.GetParent(
+            Path.GetDirectoryName(typeof(InternetExplorer).Assembly.Location)).
+                Parent.FullName + @"\Vendor";
+
+        [SetUp]
+        public void SetUp()
+        {
+            Driver = new InternetExplorerDriver(VendorDirectory);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.Quit();
+        }
+
+        [Test]
+        public void PageLoads()
+        {
+            Driver.Navigate().GoToUrl("http://the-internet.herokuapp.com");
+            Assert.That(Driver.Title.Equals("The Internet"));
+        }
+    }
+}
