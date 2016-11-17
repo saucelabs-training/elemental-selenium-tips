06f48ad367923843c857ae23371156160a2973e0
Dave Haeffner <dhaeffner@gmail.com>
2016-11-17 13:10:06 -0500

    Updated Chromedriver binaries and pull in the updated versions of the selenium-server-standalone.jar file (2.53.1 and 3.0.1). Added a Python code example for 52-grid. And updated the gitignore to cover all .swp files.

---
+++ 52-grid/python/grid.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Implementation of http://elementalselenium.com/tips/52-grid
+"""
+
+import os
+import unittest
+from selenium import webdriver
+
+
+class Grid(unittest.TestCase):
+
+    def setUp(self):
+        url = 'http://localhost:4444/wd/hub'
+        desired_caps = {}
+        desired_caps['browserName'] = 'firefox'
+        #desired_caps['browserName'] = 'chrome'
+        self.driver = webdriver.Remote(url, desired_caps)
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_page_loaded(self):
+        driver = self.driver
+        driver.get('http://the-internet.herokuapp.com')
+        assert driver.title == 'The Internet'
+
+if __name__ == "__main__":
+    unittest.main()
