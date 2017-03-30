d9c6eb4483e34d61f39847b8acf026be6a925ab6
ManojKumar Kumar <manoj9788@gmail.com>
2017-03-30 14:46:11 +1100

    [js]12-ab-opt-out-tests

---
+++ 12-opt-out-of-ab-tests/javascript/test/abOptOutTest.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const assert = require('assert');
+const {Builder, By, promise, until} = require('selenium-webdriver');
+const {fs} = require('fs-extra');
+const path = require('path');
+
+promise.USE_PROMISE_MANAGER = false;
+
+describe('AB Opt Out Tests', function() {
+  let driver;
+
+  beforeEach(async function() {
+    driver = await new Builder().forBrowser('chrome').build();
+  });
+
+  afterEach(async function() {
+    await driver.quit();
+  });
+
+  it('withCookieAfterVisitingPage', async function() {
+      async function startsWith(str, match) {
+          return str.lastIndexOf(match, 0) === 0;
+      }
+    await driver.get("http://the-internet.herokuapp.com/abtest");
+    let headingText = await driver.findElement(By.css("h3")).getText();
+    await startsWith(headingText, "A/B Test");
+    await driver.manage().addCookie({name: 'optimizelyOptOut', value: 'true'});
+    await driver.navigate().refresh();
+    headingText = await driver.findElement(By.css("h3")).getText();
+    assert.equal(headingText, "No A/B Test");
+  });
+
+  it('withCookieBeforeVisitingPage', async function() {
+  await driver.get("http://the-internet.herokuapp.com");
+  await driver.manage().addCookie({name: 'optimizelyOptOut', value: 'true'});
+  await driver.get("http://the-internet.herokuapp.com/abtest");
+  assert.equal( await driver.findElement(By.css("h3")).getText(), ("No A/B Test"));
+  });
+
+  it('withOptOutUrl', async function() {
+  await driver.get("http://the-internet.herokuapp.com/abtest?optimizely_opt_out=true");
+  await driver.switchTo().alert().dismiss();
+  assert.equal( await driver.findElement(By.css("h3")).getText(), ("No A/B Test"));
+  });
+
+
+});
