a4c9270cfbcbafdeaaeb758bbe9c513f4741b2b1
Chris Kenst <ckenst@gmail.com>
2017-10-03 22:57:23 -0700

    Added a comment

--- 72-headless-chrome/ruby/example.rb
+++ 72-headless-chrome/ruby/example.rb
@@ -6,7 +6,7 @@ def setup
   options = Selenium::WebDriver::Chrome::Options.new
   options.add_argument('--headless')
   options.add_argument('--disable-gpu')
-  options.add_argument('--remote-debuging-port=9222')
+  options.add_argument('--remote-debuging-port=9222') # Optional. If it doesn't work, change or comment out
   @driver = Selenium::WebDriver.for :chrome, options: options
 end
 
