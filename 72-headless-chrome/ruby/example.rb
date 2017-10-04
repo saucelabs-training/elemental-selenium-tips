416ad77d3a0d6eae0d5224cef80154bbd5ba7c0b
Chris Kenst <ckenst@gmail.com>
2017-10-03 22:20:59 -0700

    Ruby code example for tip 72

---
+++ 72-headless-chrome/ruby/example.rb
@@ -0,0 +1,27 @@
+require 'selenium-webdriver'
+require 'rspec/expectations'
+include RSpec::Matchers
+
+def setup
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--headless')
+  options.add_argument('--disable-gpu')
+  options.add_argument('--remote-debuging-port=9222')
+  @driver = Selenium::WebDriver.for :chrome, options: options
+end
+
+def teardown
+  @driver.quit
+end
+
+def run
+  setup
+  yield
+  teardown
+end
+
+run do
+  @driver.get 'http://the-internet.herokuapp.com'
+  expect(@driver.title).to eql "The Internet"
+  @driver.save_screenshot('headless.png')
+end
