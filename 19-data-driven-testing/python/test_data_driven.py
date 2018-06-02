d44d92113e66dc08c8b4abe51d881cfa6bac2fda
Dave Haeffner <dhaeffner@gmail.com>
2018-06-02 14:00:50 +0300

    Looks great! A couple of small tweaks. I took out the print statement, removed the unnused variable declaration, and took out the alias for expected conditions (in favor of its full name).

--- 19-data-driven-testing/python/test_data_driven.py
+++ 19-data-driven-testing/python/test_data_driven.py
@@ -8,7 +8,7 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions
 
 
 class Upload(unittest.TestCase):
@@ -28,10 +28,9 @@ class Upload(unittest.TestCase):
                 driver.find_element_by_id('username').send_keys(row['username'])
                 driver.find_element_by_id('password').send_keys(row['password'])
                 driver.find_element_by_id('login').submit()
-                element = WebDriverWait(driver, 6).until(
-                    EC.visibility_of_element_located((By.CLASS_NAME, "flash"))
+                WebDriverWait(driver, 6).until(
+                    expected_conditions.visibility_of_element_located((By.CLASS_NAME, "flash"))
                 )
-                print row['username'], row['password']
                 assert row['notification_message'] in driver.find_element_by_class_name('flash').text
 
 
