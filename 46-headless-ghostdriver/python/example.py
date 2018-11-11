b414be6bf243a66304780e41d4cedb5b00f3f0f5
spyk <spyk.developpeur@gmail.com>
2018-11-11 01:02:58 +0100

    headless ghost driver

---
+++ 46-headless-ghostdriver/python/example.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Implementation of http://elementalselenium.com/tips/46-headless-ghostdrivers
+"""
+import unittest
+
+from selenium import webdriver
+
+class LocatorStrategy(unittest.TestCase):
+
+	def setUp(self):
+		self.driver = webdriver.Remote(command_executor='http://localhost:8001')
+
+	def tearDown(self):
+		self.driver.quit()
+
+	def test_example_1(self):
+		self.driver.get('http://the-internet.herokuapp.com')
+
+		return self.driver.title == 'The Internet'
+
+if __name__ == '__main__':
+	unittest.main()
