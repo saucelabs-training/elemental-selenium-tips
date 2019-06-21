40b3585a66fd5aeb0d23aad845ec3ac0bda5d663
David Haeffner <dhaeffner@gmail.com>
2019-06-21 21:00:57 +0300

    Updated locator in example

--- 61-keyboard-keys/python/keyboard_keys.py
+++ 61-keyboard-keys/python/keyboard_keys.py
@@ -19,7 +19,7 @@ class KeyboardKeys(unittest.TestCase):
     def test_example_1(self):
         driver = self.driver
         driver.get('http://the-internet.herokuapp.com/key_presses')
-        driver.find_element_by_class_name('example').send_keys(Keys.SPACE)
+        driver.find_element_by_id('target').send_keys(Keys.SPACE)
         assert driver.find_element_by_id('result').text == 'You entered: SPACE'
         ActionChains(driver).send_keys(Keys.TAB).perform()
         assert driver.find_element_by_id('result').text == 'You entered: TAB'
